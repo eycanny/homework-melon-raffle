0a08cc09d3487584fb8db3694db06ae79ecb26c0
eycanny <nancy.the.coder@gmail.com>
2022-03-23 20:19:31 -0700

    Refactored code by importing get_customers_from_file from customers modue; removed repetitive codes Customer class and get_customers_from_file() function

--- version2/raffle.py
+++ version2/raffle.py
@@ -2,45 +2,7 @@
 
 import random
 
-class Customer:
-    """A customer at Ubermelon."""
-
-    def __init__(self, name, email, street, city, zipcode):
-        self.name = name
-        self.email = email
-        self.street = street
-        self.city = city
-        self.zipcode = zipcode
-
-    def __repr__(self):
-        """Show customer info."""
-
-        return f"Customer Name: {self.name}\nEmail: {self.email}\nStreet Address: {self.street}\nCity: {self.city}\nZipcode: {self.zipcode}"
-
-
-def get_customers_from_file(customer_file_path):
-    """Read customer file and return list of customer objects.
-
-    Read file at customer_file_path and create a customer object
-    containing customer information.
-    """
-
-    customers = []
-
-    customer_file = open(customer_file_path)
-
-    # Process a file like:
-    #
-    #   customer-name | email | street | city | zipcode
-
-    for line in customer_file:
-        customer_data = line.strip().split("|")
-        name, email, street, city, zipcode = customer_data
-
-        new_customer = Customer(name, email, street, city, zipcode)
-        customers.append(new_customer)
-
-    return customers
+from customers import get_customers_from_file
 
 
 def pick_winner(customers):
