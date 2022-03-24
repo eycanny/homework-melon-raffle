d3fcc3bab78ba2047f73cef5e7712735b434fc29
eycanny <nancy.the.coder@gmail.com>
2022-03-23 19:51:54 -0700

    Imported code from original raffle.py; added __repr__ method to class Customer; updated line 3 to be 'import random' instead of 'from random import choice' which fixed name error and raffle.py

--- version2/raffle.py
+++ version2/raffle.py
@@ -1,6 +1,61 @@
 """Randomly pick customer and print customer info"""
 
-# Add code starting here
+import random
 
-# Hint: remember to import any functions you need from
-# other files or libraries
+class Customer:
+    """A customer at Ubermelon."""
+
+    def __init__(self, name, email, street, city, zipcode):
+        self.name = name
+        self.email = email
+        self.street = street
+        self.city = city
+        self.zipcode = zipcode
+
+    def __repr__(self):
+        """Show customer info."""
+
+        return f"Customer Name: {self.name}\nEmail: {self.email}\nStreet Address: {self.street}\nCity: {self.city}\nZipcode: {self.zipcode}"
+
+
+def get_customers_from_file(customer_file_path):
+    """Read customer file and return list of customer objects.
+
+    Read file at customer_file_path and create a customer object
+    containing customer information.
+    """
+
+    customers = []
+
+    customer_file = open(customer_file_path)
+
+    # Process a file like:
+    #
+    #   customer-name | email | street | city | zipcode
+
+    for line in customer_file:
+        customer_data = line.strip().split("|")
+        name, email, street, city, zipcode = customer_data
+
+        new_customer = Customer(name, email, street, city, zipcode)
+        customers.append(new_customer)
+
+    return customers
+
+
+def pick_winner(customers):
+    """Choose a random winner from list of customers."""
+
+    chosen_customer = random.choice(customers)
+
+    name = chosen_customer.name
+    email = chosen_customer.email
+
+    print(f"Tell {name} at {email} that they've won.")
+
+
+def run_raffle():
+    """Run the weekly raffle."""
+
+    customers = get_customers_from_file("customers.txt")
+    pick_winner(customers)
